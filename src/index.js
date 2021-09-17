a322886e06d5129c4d42684941419e988312088f
MaxusGuitar <maxim1999chikhanov@gmail.com>
2021-09-17 23:50:24 +0300

    template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './sass/main.scss';
+import './sass/main.scss'
+import menu from './../menu.json'
+import menuTemplate from './../templates/menu.hbs'
+
+const menuJS = document.querySelector('.menu')
+const menuCreate = createAllMenu(menu)
+
+menuJS.insertAdjacentHTML('beforeend', menuCreate)
+
+function createAllMenu(menu) {
+    return menu.map(menuTemplate).join('')
+}
