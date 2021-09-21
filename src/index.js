6f386b9ded0be7fea993ba8c9b20dd553c836122
MaxusGuitar <maxim1999chikhanov@gmail.com>
2021-09-21 23:44:10 +0300

    the end

--- src/index.js
+++ src/index.js
@@ -4,9 +4,33 @@ import menuTemplate from './../templates/menu.hbs'
 
 const menuJS = document.querySelector('.menu')
 const menuCreate = createAllMenu(menu)
+const checkBox = document.querySelector('#theme-switch-toggle');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+}
+const {LIGHT, DARK} = Theme
 
 menuJS.insertAdjacentHTML('beforeend', menuCreate)
 
 function createAllMenu(menu) {
     return menu.map(menuTemplate).join('')
 }
+
+document.body.classList.add(
+  localStorage.getItem('theme') === null ? LIGHT : localStorage.getItem('theme'),
+);
+if (document.body.classList.value === DARK) {
+    checkBox.checked = true;
+}
+function changeTheme(a) {
+  if (a.target.checked) {
+    document.body.classList.replace(LIGHT, DARK);
+    localStorage.setItem('theme', DARK);
+    return;
+  }
+  document.body.classList.replace(DARK, LIGHT);
+  localStorage.setItem('theme', LIGHT);
+}
+checkBox.addEventListener('change', changeTheme);
